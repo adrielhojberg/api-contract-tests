092eb524d50816cf7962ef90f112f9a700335bf1
bonnce <bonce_reloko@hotmail.com>
2023-01-05 20:00:36 -0300

    include all src in tsconfig

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,32 +1,32 @@
-import { iLogger } from "../misc/types";
+import { iLogger } from '../misc/types'
 
-export const violationsHandler = (logger:iLogger, input:boolean) => {
-    const data = input ? 'response' : 'request'
-    const dataType = input ? 'input' : 'output'
-    logger.testData[data] = {};
-    logger.testData[data].message = [];
-    violations[dataType].forEach((violation) => {
-    const code = violation.code;
-    const message = violation.message;
-    const path = violation.path;
+export const violationsHandler = (logger: iLogger, input: boolean) => {
+  const data = input ? 'response' : 'request'
+  const dataType = input ? 'input' : 'output'
+  logger.testData[data] = {}
+  logger.testData[data].message = []
+  violations[dataType].forEach((violation) => {
+    const code = violation.code
+    const message = violation.message
+    const path = violation.path
     logger.testData[data].message = [
-        ...logger.testData[data].message,
-        `${code || ""} - ${message} - ${path || ""}`,
-    ];
-    });
+      ...logger.testData[data].message,
+        `${code || ''} - ${message} - ${path || ''}`
+    ]
+  })
 }
 
-export const logger_testData =  async (response, error) => {
-    const logger:iLogger = {};
-    let violations = response.violations;
+export const logger_testData = async (response, error) => {
+  const logger: iLogger = {}
+  const violations = response.violations
 
-    logger["testData"] = {};
-    if (violations.input.length >= 1) {
-        violationsHandler(logger, true)
-    }
-    if (violations.output.length >= 1) {
-        violationsHandler(logger, false)
-    }
+  logger.testData = {}
+  if (violations.input.length >= 1) {
+    violationsHandler(logger, true)
+  }
+  if (violations.output.length >= 1) {
+    violationsHandler(logger, false)
+  }
 
-    return logger;
+  return logger
 }
--- src/contract/individualTest.ts
+++ src/contract/individualTest.ts
@@ -1,14 +1,14 @@
-import runContractTests from "./runner";
+import runContractTests from './runner'
 
-export default async function individualTest(operation, client) {
-    let response;
-    try {
-        response = await runContractTests(operation, client);
+export default async function individualTest (operation, client) {
+  let response
+  try {
+    response = await runContractTests(operation, client)
 
-        await response.violations.input.should.have.lengthOf(0);
-        await response.violations.output.should.have.lengthOf(0);
-    } catch (error) {
-        const errorLogger = await Utils.logger_testData(response, error);
-        throw errorLogger;
-    }
+    await response.violations.input.should.have.lengthOf(0)
+    await response.violations.output.should.have.lengthOf(0)
+  } catch (error) {
+    const errorLogger = await Utils.logger_testData(response, error)
+    throw errorLogger
+  }
 }
