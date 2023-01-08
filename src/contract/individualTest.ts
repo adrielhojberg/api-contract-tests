7e1daf136ab94b3f92c35419c2fb8bb25e766582
Adriel Hojberg <adrielhojberg@gmail.com>
2023-01-08 16:28:52 -0300

    updated chai assertions to work

--- src/contract/individualTest.ts
+++ src/contract/individualTest.ts
@@ -2,7 +2,8 @@ import { PrismHttp } from '@stoplight/prism-http/dist/client'
 import { IHttpOperation } from '@stoplight/types'
 import { loggerTestData } from '../common/logger'
 import runContractTests from './runner'
-import 'chai'
+import chai from 'chai'
+chai.should()
 
 export default async function individualTest (operation: IHttpOperation<false>, client: PrismHttp): Promise<void> {
   // let response
