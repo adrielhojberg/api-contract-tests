74e215f71b96e9d3528dab7f6955989e5c3872e0
Adriel Hojberg <adrielhojberg@gmail.com>
2023-01-11 12:11:43 -0300

    fixed alternative example

--- src/common/treatPath.ts
+++ src/common/treatPath.ts
@@ -5,13 +5,13 @@ import { PATHVARREGEXP } from '../misc/const'
 export const treatPath = async (operation: IHttpOperation<false>): Promise<string> => {
   if (PATHVARREGEXP.test(operation.path)) {
     const example = operation.request?.path?.[0]?.examples?.[0]
-    const altExample = operation.request?.path?.[0]?.schema?.examples
+    const altExample = operation.request?.path?.[0]?.schema?.examples as Array<string | number | boolean>
     const value = (example != null) && 'value' in example && (typeof example.value === 'string' || typeof example.value === 'number') ? example.value.toString() : undefined
     if (value != null) {
       return pathReplaceVar(operation.path, value)
     }
     if (altExample != null) {
-      const value = altExample[0]?.toString() ?? ''
+      const value: string = altExample?.toString() ?? ''
       return pathReplaceVar(operation.path, value)
     }
   }
