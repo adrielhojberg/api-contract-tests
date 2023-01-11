ff217f7db7580c7268459c9b22bd96435b663f07
Adriel Hojberg <adrielhojberg@gmail.com>
2023-01-11 11:35:26 -0300

    added alternative example handling

--- src/common/treatPath.ts
+++ src/common/treatPath.ts
@@ -5,8 +5,13 @@ import { PATHVARREGEXP } from '../misc/const'
 export const treatPath = async (operation: IHttpOperation<false>): Promise<string> => {
   if (PATHVARREGEXP.test(operation.path)) {
     const example = operation.request?.path?.[0]?.examples?.[0]
+    const altExample = operation.request?.path?.[0]?.schema?.examples
     const value = (example != null) && 'value' in example && (typeof example.value === 'string' || typeof example.value === 'number') ? example.value.toString() : undefined
     if (value != null) {
+      return pathReplaceVar(operation.path, value)
+    }
+    if (altExample != null) {
+      const value = altExample[0]?.toString() ?? ''
       return pathReplaceVar(operation.path, value)
     }
   }
