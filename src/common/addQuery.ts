42820b21b2cae9bec1eec2b1cfd47be41f3d37a7
bonnce <bonce_reloko@hotmail.com>
2023-01-12 21:19:43 -0300

    http response error handler added, and fix error

--- src/common/addQuery.ts
+++ src/common/addQuery.ts
@@ -4,7 +4,7 @@ export function addQuery (query: Array<IHttpQueryParam<false>>, path: string): s
   let newPath: string = path + '?'
   query.forEach((q, i) => {
     const example = q.schema?.examples as Array<string | number | boolean>
-    const newQuery = `${q.name}=${example[0].toString()}`
+    const newQuery = `${q.name}=${example?.[0]?.toString()}`
     newPath += newQuery
     if (i + 1 !== query.length) {
       newPath += '&'
--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -1,5 +1,14 @@
 import { PATHVARREGEXP } from './const'
+import { ProblemJson } from '@stoplight/prism-http'
 
 export const pathReplaceVar = (str: string, toReplace: string): string => {
   return str.replace(PATHVARREGEXP, toReplace)
+}
+
+export const jsonErrorHandler = (err: ProblemJson & Partial<Error>): never => {
+  const jsonError = err.type === 'invalid-json'
+    ? 'The body type and the header type doesn\'t match, you must send a valid json.\n'
+    : ''
+  const message = jsonError + (err.message ?? err.detail)
+  throw new Error(message)
 }
