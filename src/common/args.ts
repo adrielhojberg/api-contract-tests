34059cfd72468aa265f367d55bb4889dab98105b
bonnce <bonce_reloko@hotmail.com>
2023-02-04 18:39:49 -0300

    example and description added to cli args

--- src/common/args.ts
+++ src/common/args.ts
@@ -1,9 +1,22 @@
 import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
+
 const argv = yargs(hideBin(process.argv)).options({
-  f: { type: 'string', demandOption: true, alias: 'file' },
-  s: { type: 'string', alias: 'server' }
+  f: {
+    type: 'string',
+    describe: 'Dir path to the yaml file',
+    demandOption: true,
+    alias: 'file'
+  },
+  s: {
+    type: 'string',
+    describe: 'Custom server url to use on every contract test',
+    alias: 'server'
+  }
 }
-).parseSync()
+).example([
+  ['$0 -f "./example.yaml"', 'Use your yaml dir path'],
+  ['$0 -s "http://localhost:8080"', 'Put your server url']
+]).parseSync()
 
 export default argv
